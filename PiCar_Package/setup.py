2757308eb99856eeec8da11db91153cb0b959d47
AlexAntonyN <alex.nedumparambil@in-tech.com>
2022-10-11 15:05:02 +0200

    Deleted removed files

--- PiCar_Package/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-requirements = []
-setup(
-    name="virtual_picar",
-    version="1.0.0",
-    description="PiCar library",
-    packages=find_packages(),
-    install_requires=requirements
-)
--- start_model.bat
+++
@@ -1,3 +0,0 @@
-cd PiCar_Package
-
-C:\Users\U4793\.conda\envs\Virtuelmodel\python.exe start_model.py
